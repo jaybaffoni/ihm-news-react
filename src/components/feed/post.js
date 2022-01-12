39adb6e2816f448aed694973ee9e18462ec67ff6
Nancy B <nancybohane@gmail.com>
2022-01-12 16:54:01 -0500

    inline styling for posts

--- src/components/feed/post.js
+++ src/components/feed/post.js
@@ -4,15 +4,38 @@ import Avatar from "../common/avatar";
 const Post = ({postData}) => {
 
     let {id, avatar, timeStamp, displayName, edited, text} = postData;
-  
+    
+    const boxStyle = {
+        color: "White",
+        backgroundColor: "DarkBlue",
+        textAlign: "center",
+        padding: "35px",
+      };
+
+      const displayNameStyle = {
+          fontWeight: "bold",
+          color: "Yellow"
+      };
+
+      const textStyle = {
+            fontStyle: "Arial",
+      };
+
+      const timeStampStyle = {
+            fontSize: "12px",
+            color: "gray"
+      };
+     
     return (
         <div>
-            <p>id: {id}</p>
-            <Avatar source={avatar}/>
-            <p>timeStamp: {timeStamp}</p>
-            <p>displayName: {displayName}</p>
-            <p>edited: {edited}</p>
-            <p>text: {text}</p>
+            <div container style={boxStyle}>
+                <p>{id}</p>
+                <Avatar source={avatar}/>
+                <p style={displayNameStyle}>{displayName}</p>
+                <p style={textStyle}>{text}</p>
+                <p style={timeStampStyle}>{timeStamp}</p>
+                <p>edited: {edited}</p>
+            </div>
         </div>
     )
 };
