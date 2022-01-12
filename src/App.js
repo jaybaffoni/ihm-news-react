bc5df8c058fa2f854dcffe71bd8765220b7f1273
Nancy B <nancybohane@gmail.com>
2022-01-12 11:40:27 -0500

    Avatar Update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 import CompanyLogo from './components/common/company-logo';
 import {AppButton} from './components/common/app-button';
-import Button from 'react-bootstrap/Button';
+import Avatar from './components/common/avatar';
 
 function App() {
   return (
     <div>
-      <h1>Nancy Test</h1>
-      <CompanyLogo/>
-      <CompanyLogo size='medium' rounded/>
-      <CompanyLogo size='small'/>
-      <AppButton variant="primary" title="Press Me"/>
-      <AppButton variant="secondary" title="Press Me"/>
-      <AppButton variant="light" title="Press Me"/>
-      <AppButton variant="dark" title="Press Me"/>
+      <Avatar/>
+      <Avatar source="https://picsum.photos/200"/>
     </div>
   );
 }
--- src/components/common/avatar.js
+++ src/components/common/avatar.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { config } from "./config";
+import companyLogo from "../../images/company-logo.jpg"
 
-const avatar = ({size='small', rounded=true}) => {
+const Avatar = ({size='xs', rounded=true, source=companyLogo}) => {
     return (
       <div>
-        <img style={{width:config.sizes[size], borderRadius:rounded ? '50%' : 0}} src={avatar} alt={"Avatar"}/>
+        <img style={{width:config.sizes[size], borderRadius:rounded ? '50%' : 0}} src={source} alt={"Avatar"}/>
       </div>
     )
   };
 
-export default avatar;
+export default Avatar;
